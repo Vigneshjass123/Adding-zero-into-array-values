1bd57df0ff9a0edb53f43cedeb513b8ff0dbbd8d
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-10 02:09:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_Diffrence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //input ={1,2,3,4,5}
+            //following output = {1020304050}
+
+            int[] input = { 1, 2, 3, 4, 5 };
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Console.Write(input[i] + "0");
+                }
+            }
+        }
+    }
+}
+            
+ 
+
+
+
+                 
+
+
+
+       
